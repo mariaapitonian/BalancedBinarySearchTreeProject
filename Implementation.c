947447b5e5fd0d879de144084463b7018c62d5a1
mariaapitonian <maria.apitonian2002@gmail.com>
2021-11-29 17:07:12 +0400

    Search, Height functions

--- Implementation.c
+++ Implementation.c
@@ -26,9 +26,58 @@ struct node* node_create(element_type key, struct node* left, struct node* right
 	//To do
 }
 
-struct tree* tree_create()
+struct tree* tree_create(element_type data, struct node* left, struct node* right)
 {
 	//To do
+}
+
+int tree_height(struct tree* t)
+{
+	if(t->start == NULL){
+		return 0;
+	}
+
+	int maxleft = 1;
+	int maxright = 1;
+	struct node* leftsub = node_create(1, NULL, NULL);
+	struct node* rightsub = node_create(1, NULL, NULL);
+	leftsub = t->start;
+	rightsub = t->start;
+	
+	while(true){
+		if(leftsub->left !== NULL){
+			leftsub = leftsub->left;
+			maxleft++;
+		}
+		else if(leftsub->right !== NULL){
+			leftsub = leftsub->right;
+			maxleft++;
+		}
+		else{
+			break;
+		}
+	}
+	while(true){
+		if(rightsub->right !== NULL){
+			rightsub = rightsub->right;
+			maxright++;
+		}
+		else if(rightsub->left !== NULL){
+			rightsub = rightsub->left;
+			maxright++;
+		}
+		else{
+			break;
+		}
+	}
+
+	if(maxleft>maxright){
+		return maxleft;
+	}
+	else{
+		return maxright;
+	}
+	
 }
 
 void tree_insert()
@@ -39,4 +88,38 @@ void tree_insert()
 void tree_delete()
 {
 	//To do
+}
+
+bool search_tree(struct tree* t, element_type value)
+{
+	root = t->start;
+	char path[tree_height(t)] = {};
+	int steps = 0;
+
+	while(int i=0; i<tree_height(t); i++) {			//write function for height
+		temp=root;
+		if(root==value){
+			if(steps=0){
+				printf("The values exists at the root position");
+			}
+			else{
+				printf("The value exists at position %s", path);
+			}
+			return 1;
+		}
+		else if((root<value) && (temp->left !== NULL)){
+			root=root->left;
+			path[steps]="left ";
+			steps++;
+		}
+		else if((root<value) && (temp->right !== NULL)){
+			root=root->right;
+			path[steps]="right ";
+			steps++;
+		}
+		else{
+			printf("The value does not exist in the binary search tree");
+			return 0;
+		}
+	}
 }
